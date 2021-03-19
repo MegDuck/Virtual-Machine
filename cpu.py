736d176e476ca4e44fe7e7e609529b2be646ac3b
MegDuck <vsyakayavsyachina1234@gmail.com>
2021-03-19 15:33:45 +0800

    some reworks and 'out' command for output

--- cpu.py
+++ cpu.py
@@ -1,9 +1,10 @@
-import colorama
+
+import sys
 
 
 
 
-DEBUG = True
+DEBUG = False
 
 def log(message):
     if DEBUG==True:
@@ -11,6 +12,7 @@ def log(message):
 
 
 def exec_program(comp, program):
+
     ip = 0
     while True:
         try:
@@ -28,6 +30,7 @@ def exec_program(comp, program):
             popped = comp.regs[program[ip+1]]
             comp.regs[program[ip+1]] = 0
             log(f"Pop the value {popped} of register {program[ip+1]}")
+
         elif command == "add":
             first = comp.regs[program[ip+1]]
             second = comp.regs[program[ip+2]]
@@ -36,6 +39,7 @@ def exec_program(comp, program):
             result = first + second
             log(f"Add {first} to {second} and push it to register {program[ip+2]}")
             comp.regs[program[ip+2]] = result
+            ip += 2
         elif command == "goto":
             ip = program[ip+1]
             log(f"We now at {ip}")
@@ -50,9 +54,23 @@ def exec_program(comp, program):
             second = comp.regs[program[ip+2]]
             if first != second:
                 ip += 3
+            ip += 2
         elif command == "move":
             first = comp.regs[program[ip+1]]
             comp.regs[program[ip+2]] = first
+            ip += 2
+        elif command == "put":
+            message = program[ip + 1]
+            output = ""
+            if message in list(comp.regs):
+                output = comp.regs[message]
+                log(f"putting {output} from {message}")
+            else:
+                output = program[ip+1]
+                log(f"Putting {output}")
+
+            sys.stdout.write(str(output))
+            ip += 1
         ip += 1
         
 
@@ -63,8 +81,9 @@ class computer:
         self.regs = {"A":0, "B":0, "C":0, "D":0, "E":0}
 
 
-
-program = ["push", "C", 20,  "move", "A", "B", "push", "A", 5, "add", "B", "A", "equal", "A", "C", "goto", 3, "halt"]
+program = ["push", "C", 20,  "move", "A", "B", "push", "A", 5, "add", "B", "A",
+        "equal", "A", "C", "goto", 3, "put", "Value from register A: ", "put",
+        "A", "put", "\n", "halt"]
 
 
 mcp = computer()
